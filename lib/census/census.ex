b1ef9eb1c9f01d27f9fbdb3ccecf7f72496bf4cc
Andrés Felipe López Ferro <andreslopezferro@gmail.com>
2018-10-12 13:37:08 -0500

    feat: Fix timeout for httpoison

--- lib/census/census.ex
+++ lib/census/census.ex
@@ -37,7 +37,7 @@ defmodule Census do
   end
 
   defp run_request(request) do
-    {:ok, %HTTPoison.Response{body: body, headers: _headers}} = HTTPoison.post('https://uscensus.prod.3ceonline.com/ui/classify', request, [{"Content-Type", "application/json"}])
+    {:ok, %HTTPoison.Response{body: body, headers: _headers}} = HTTPoison.post('https://uscensus.prod.3ceonline.com/ui/classify', request, [{"Content-Type", "application/json"}, timeout: 50_000, recv_timeout: 50_000])
     Poison.decode!(body)
   end
 end
